1b16f1b4023dbdf6540df6563d2791cb732645a0
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-11-02 12:10:34 +0300

    Update favicon

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -14,6 +14,7 @@ function Layout(props: PropsWithChildren<unknown>) {
     >
       <Head>
         <title>Next.js Templates | Free Templates and UI kits</title>
+        <link rel="sortcut icon" href="favicon.ico" />
       </Head>
       <Header />
       <Flex
