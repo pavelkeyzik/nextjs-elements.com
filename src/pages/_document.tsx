4a6f98aa37cccfa723d9a1d19c8a23a6603ec9ac
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-11-04 11:20:12 +0300

    Add Open Graph Tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,6 +26,39 @@ class MyDocument extends Document {
           />
           <meta name="author" content="Pavel Keyzik" />
           <link rel="sortcut icon" href="/favicon.ico" />
+          <meta
+            itemProp="name"
+            content="Next.js Elements | Free Templates and Boilerplates"
+          />
+          <meta
+            itemProp="description"
+            content="Free Next.js Templates, Boilerplates, and Starter Kits for your website."
+          />
+          <meta itemProp="image" content="/assets/meta/card-image.png" />
+
+          <meta property="og:url" content="https://www.nextjs-elements.com" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            content="Next.js Elements | Free Templates and Boilerplates"
+          />
+          <meta
+            property="og:description"
+            content="Free Next.js Templates, Boilerplates, and Starter Kits for your website."
+          />
+          <meta property="og:image" content="/assets/meta/card-image.png" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:title"
+            content="Next.js Elements | Free Templates and Boilerplates"
+          />
+          <meta
+            name="twitter:description"
+            content="Free Next.js Templates, Boilerplates, and Starter Kits for your website."
+          />
+          <meta name="twitter:image" content="/assets/meta/card-image.png" />
+
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-Q4T6HYR0JP"
